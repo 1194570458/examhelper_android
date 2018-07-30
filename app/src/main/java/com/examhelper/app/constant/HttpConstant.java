c7d994b69f06610d9cb5fd08d27fa8f779d07b9f
Jun <274339699@qq.com>
2018-07-30 13:29:11 +0800

    下拉 无更新

--- app/src/main/java/com/examhelper/app/constant/HttpConstant.java
+++ app/src/main/java/com/examhelper/app/constant/HttpConstant.java
@@ -5,7 +5,8 @@ package com.examhelper.app.constant;
  */
 
 public class HttpConstant {
-    private static final String CONNECTION_SERVICE_ADDRESS = "http://192.168.137.1:8000/";//服务器地址
+//    private static final String CONNECTION_SERVICE_ADDRESS = "http://192.168.137.1:8000/";//服务器地址
+    private static final String CONNECTION_SERVICE_ADDRESS = "http://10.32.142.189:8080/";//Jun服务器地址
     public static final String API_SYNTHESIZES = CONNECTION_SERVICE_ADDRESS + "synthesizes";//获取所有题目
     public static final String API_PROPERTYS = CONNECTION_SERVICE_ADDRESS + "propertys";//获取所有章节
 
--- app/src/main/java/com/examhelper/app/listener/LoginToMainListener.java
+++ app/src/main/java/com/examhelper/app/listener/LoginToMainListener.java
@@ -4,7 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 
-import com.examhelper.app.ui.activity.MainActivity;
+import com.examhelper.app.constant.IntentFlagConstant;
+import com.examhelper.app.entity.Question;
+import com.examhelper.app.service.IQuestionService;
+import com.examhelper.app.service.imp.QuesionServiceImp;
+import com.examhelper.app.ui.activity.AnalogyExaminationActivity;
+
+import java.io.Serializable;
+import java.util.List;
 
 
 public class LoginToMainListener implements View.OnClickListener {
@@ -17,13 +24,13 @@ public class LoginToMainListener implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-//        Intent intent = new Intent(context, AnalogyExaminationActivity.class);
-//        IQuestionService iQuestionService = new QuesionServiceImp(v.getContext());
-//        List<Question> questions = iQuestionService.queryAllQuestions();
-//        intent.putExtra(IntentFlagConstant.GET_QUESTIONS, (Serializable) questions);
-//        intent.putExtra(IntentFlagConstant.IS_EXMA, true);
-//        context.startActivity(intent);
-        Intent intent=new Intent(context, MainActivity.class);
+        Intent intent = new Intent(context, AnalogyExaminationActivity.class);
+        IQuestionService iQuestionService = new QuesionServiceImp(v.getContext());
+        List<Question> questions = iQuestionService.queryAllQuestions();
+        intent.putExtra(IntentFlagConstant.GET_QUESTIONS, (Serializable) questions);
+        intent.putExtra(IntentFlagConstant.IS_EXMA, true);
         context.startActivity(intent);
+//        Intent intent=new Intent(context, MainActivity.class);
+//        context.startActivity(intent);
     }
 }
